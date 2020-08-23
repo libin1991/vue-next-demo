8f5c405cd2c2ee966d8f67e8aabc49bc4ac67afa
dolymood <dolymood@gmail.com>
2020-08-23 22:02:58 +0800

    opti

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,6 @@ module.exports = {
   publicPath: '/vue-next-demo/',
   transpileDependencies: [/@vue\/.*/, 'vue-router', 'vue'],
   chainWebpack: config => {
-    // config
-    //   .devtool(false)
     config.resolve.alias.set('@vue/reactivity', 'vue-reactivity-with-polyfill')
-    config.optimization.minimizers.delete('terser')
   }
 }
